49ed5f1ce8b92959c04f478e481cd9f00fbb70e4
Abraham Ayala <abraham.ayala@ixxus.com>
2017-11-03 16:54:11 +0100

    More fixes to scripts

--- ci/build-global-lib-for-all-modules.sh
+++ ci/build-global-lib-for-all-modules.sh
@@ -12,6 +12,7 @@ mvn -s git-repo-all-out/ci/settings.xml -f git-repo-all-out/global-lib-for-all-m
 echo "Commiting changes..."
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
+touch ~/.ssh/known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 cd git-repo-all-out
 git add .
